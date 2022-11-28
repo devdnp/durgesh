2108b9ef8f79c8fbbeebf4c5ef16184e67dabfd0
durgeshvipul <durgeshpandey419@gmail.com>
2022-11-28 22:28:33 +0530

    Prime number program

---
+++ Prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prime{
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.print("enter number: ");
+     int n = sc.nextInt();	
+
+	System.out.println("Prime numbers between 1 and	" + n);
+
+    		//loop	through	the	numbers	one	by	one	
+		for	(int i=1; i < n; i++){	
+        boolean	isPrime	= true;	
+			//check	to	see	if	the	number	is	prime	
+		for	(int j=2; j<i; j++){	
+			if(i%j == 0){	
+				isPrime	= false;	
+			    break;	
+	 	 	 	}
+	 	 }	
+			//	print	the	number	
+					if (isPrime)	
+                    System.out.print(i+" ");	
+	 }
+    }
+}
